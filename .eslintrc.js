af9c9e5011f4365f8c47058973997e29f43ce24b
NikolayKaurov <nikolaykaurov@gmail.com>
2022-09-02 09:16:01 +0700

    eslint installed

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-use-before-define': [
+      'error',
+      {
+        functions: false,
+      },
+    ],
+  },
+};
